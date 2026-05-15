5d2cf8401e292046eafd18a106164e8559426464
aa15818677864 <aa15818677864@gmail.com>
2026-05-15 16:27:34 +0800

    Route form submissions through relay endpoint

--- site-config.js
+++ site-config.js
@@ -1,3 +1,3 @@
 window.SITE_CONFIG = {
-  googleSheetsEndpoint: "https://script.google.com/macros/s/AKfycbycXbwaVJjiEYKROV9IfxquO3DlN42_LTX-WMJwMCuUl0CdsRpSj47KjaU4fyBjow4/exec"
+  googleSheetsEndpoint: "https://submit.jingwei-law.com/submit.php"
 };
