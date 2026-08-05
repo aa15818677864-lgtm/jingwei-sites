d0ab6ace1a52cdfd22e1a7d2cf0396080d45978b
aa15818677864 <aa15818677864@gmail.com>
2026-08-05 12:13:11 +0800

    feat: add Liu Yi criminal landing page

---
+++ xs-assets/config.js
@@ -0,0 +1,8 @@
+window.LAWYER_SITE_CONFIG = {
+  lawyerName: "劉毅",
+  lawFirm: "",
+  siteCode: "liuyi-criminal-xs",
+  siteLabel: "劉毅律師內地刑事案件方向釐清專題",
+  formEndpoint: "https://submit.jingwei-law.com/submit.php",
+  conversionId: "AW-18003046397/yLGoCM6KjowcEP3fwohD"
+};
