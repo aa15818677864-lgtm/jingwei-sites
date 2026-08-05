48f870b0f3562c7dc896358dd67f67a32414e093
aa15818677864 <aa15818677864@gmail.com>
2026-08-05 10:17:18 +0800

    feat: add Liu Yi divorce landing page

---
+++ lh-assets/config.js
@@ -0,0 +1,8 @@
+window.LAWYER_SITE_CONFIG = {
+  lawyerName: "劉毅",
+  lawFirm: "",
+  siteCode: "liuyi-divorce-lh",
+  siteLabel: "劉毅律師內地與香港離婚及財產分割專頁",
+  formEndpoint: "https://submit.jingwei-law.com/submit.php",
+  conversionId: "AW-18003046397/yLGoCM6KjowcEP3fwohD"
+};
